c82b75f1862f7c0f24c86ee23bdb7e19eeef80af
howardyun <howard.yun@outlook.com>
2021-05-21 14:20:14 +0800

    l

--- lists/views.py
+++ lists/views.py
@@ -5,9 +5,6 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method=='POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request,'home.html')
     # else:
     #     new_item_text=''
@@ -28,3 +25,7 @@ def test_can_save_a_POST_request(self):
 def view_list(request):
     items=Item.objects.all()
     return render(request,'list.html',{'items':items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
