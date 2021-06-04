9ecb1f641d6a6d731bb3984fe000234ad9f3b355
云绍轩 <1740010798@qq.com>
2021-06-04 14:18:13 +0800

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -12,6 +12,27 @@ class NewVisitorTest(LiveServerTestCase):  # 把测试写成类，它继承unitt
   def tearDown(self):  # tearDown: 测试之后运行的特殊方法，用来停止浏览器
     self.browser.quit()
 
+  def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1:testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+        inputbox.location['x'] + inputbox.size['width'] / 2,
+          512,
+          delta=10
+        )
     
   def wait_for_row_in_list_table(self,row_text):
         start_time=time.time()
